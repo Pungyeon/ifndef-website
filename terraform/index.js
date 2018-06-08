89e463e6694cbca52d4cb739a9680913af15a6a4
Lasse Martin Jakobsen <lasse@tengen.dk>
2018-06-08 18:46:31 +0200

    index.js is now creating a new database table

--- terraform/index.js
+++ terraform/index.js
@@ -1,9 +1,51 @@
+var AWS = require('aws-sdk');
+
+AWS.config.update({region: 'us-east-1'});
+
+var db = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+
+var params = {
+  AttributeDefinitions: [
+    {
+      AttributeName: 'ARTICLE_ID',
+      AttributeType: 'N'
+    },
+    {
+      AttributeName: 'ARTICLE_DATE',
+      AttributeType: 'S'
+    }
+  ],
+  KeySchema: [
+    {
+      AttributeName: 'ARTICLE_ID',
+      KeyType: 'HASH'
+    },
+    {
+      AttributeName: 'ARTICLE_DATE',
+      KeyType: 'RANGE'
+    }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 1,
+    WriteCapacityUnits: 1
+  },
+  TableName: 'IFNDEF_ARTICLES',
+  StreamSpecification: {
+    StreamEnabled: false
+  }
+};
+
 exports.handler = function(event, context, callback) {
+    
+  db.createTable(params, function(err, data) {
+    if (err) callback(null, { statusCode: '500', body: err });
+    
     callback(null, {
       statusCode: '200',
-      body: JSON.stringify({ 'message': 'hello world' }),
+      body: data,
       headers: {
         'Content-Type': 'application/json',
       },
     });
-  };
+  });     
+};
