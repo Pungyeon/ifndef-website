e9a76e892d2d0b059af0da50ef73421c81638c6f
pungy <lja@credocom.dk>
2018-06-08 20:25:59 +0200

    index.js now returns a list of all items in dynamodb IFNDEF_ARTICLES table

--- terraform/index.js
+++ terraform/index.js
@@ -2,42 +2,15 @@ var AWS = require('aws-sdk');
 
 AWS.config.update({region: 'us-east-1'});
 
-var db = new AWS.DynamoDB({apiVersion: '2012-10-08'});
+var db = new AWS.DynamoDB.DocumentClient();
 
 var params = {
-  AttributeDefinitions: [
-    {
-      AttributeName: 'ARTICLE_ID',
-      AttributeType: 'N'
-    },
-    {
-      AttributeName: 'ARTICLE_DATE',
-      AttributeType: 'S'
-    }
-  ],
-  KeySchema: [
-    {
-      AttributeName: 'ARTICLE_ID',
-      KeyType: 'HASH'
-    },
-    {
-      AttributeName: 'ARTICLE_DATE',
-      KeyType: 'RANGE'
-    }
-  ],
-  ProvisionedThroughput: {
-    ReadCapacityUnits: 1,
-    WriteCapacityUnits: 1
-  },
-  TableName: 'IFNDEF_ARTICLES',
-  StreamSpecification: {
-    StreamEnabled: false
-  }
-};
+  TableName: "IFNDEF_ARTICLES"
+}
 
 exports.handler = function(event, context, callback) {
     
-  db.createTable(params, function(err, data) {
+  db.scan(params, function(err, data) {
     if (err) callback(null, { statusCode: '500', body: err });
     
     callback(null, {
